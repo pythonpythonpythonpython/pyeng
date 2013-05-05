881c0be479189b6b261f6657a64937df47bd9f9f
pythonpythonpythonpython <pythonpythonpythonpython@gmail.com>
2013-05-05 16:45:18 +0200

    start

---
+++ core.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+class AppException(Exception):
+    pass
---
+++ pyeng/settings.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from os.path import dirname, realpath, join
+
+from django.core.urlresolvers import reverse_lazy
+
+PROJECT_DIR = dirname(dirname(realpath(__file__)))
+TIME_ZONE = 'Europe/Warsaw'
+LANGUAGE_CODE = 'pl'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (join(PROJECT_DIR, 'static'),)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+ROOT_URLCONF = 'pyeng.urls'
+WSGI_APPLICATION = 'pyeng.wsgi.application'
+TEMPLATE_DIRS = (join(PROJECT_DIR, 'templates'),)
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin'
+)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+from local_settings import *
